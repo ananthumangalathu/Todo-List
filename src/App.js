1165aadb747e7d2b61c7a972ce7c6fb599cff6e0
ananthumangalathu <ananthumangalathu@gmail.com>
2021-06-02 06:51:00 +0530

    added component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import todoList from './components/todoList/todoList';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <todoList/>
     </div>
   );
 }
---
+++ src/components/todoList/todoList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function todoList() {
+    return (
+        <div className="todo-container">
+            <h1>Hello World</h1>
+            {/*<div className="todo-header">
+                <h1>Todo List</h1>
+            </div>
+            <div className="todo-form">
+                <div className="todo-form-textfield">
+                    <input type="text" placeholder="Enter Todos" name="" id="" />
+                </div>
+                <div className="todo-form-button">
+                    <input type="button" value="Add" />
+                </div>
+            </div>*/}
+        </div>
+    )
+}
+
+export default todoList 
