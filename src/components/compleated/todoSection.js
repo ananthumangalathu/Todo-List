7b9e65ec30f519994e05ced4f3f4783250cf645b
ananthumangalathu <ananthumangalathu@gmail.com>
2021-06-04 17:13:44 +0530

    todo content added

--- src/components/compleated/todoSection.js
+++ src/components/compleated/todoSection.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import './TodoSection.css'
 
 function TodoSection() {
     return (
         <div>
-            {/*<h1>Hello World</h1>*/}
+            <div className="container">
+                <div className="container-header">
+                    <h1>ToDo</h1>
+                </div>
+                <div className="container-content">
+                    <div className="container-content-para">
+                    <p>this is my todo</p>
+                    </div>
+                    <div className="container-content-icon">
+                    <i class="fas fa-times"></i>
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-check"></i>
+                    </div>    
+                </div>
+                <div className="container-content">
+                    <div className="container-content-para">
+                    <p>this is my todo</p>
+                    </div>
+                    <div className="container-content-icon">
+                    <i class="fas fa-times"></i>
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-check"></i>
+                    </div>    
+                </div>
+                <div className="container-content">
+                    <div className="container-content-para">
+                    <p>this is my todo</p>
+                    </div>
+                    <div className="container-content-icon">
+                    <i class="fas fa-times"></i>
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-check"></i>
+                    </div>    
+                </div>
+            </div>
         </div>
     )
 }
